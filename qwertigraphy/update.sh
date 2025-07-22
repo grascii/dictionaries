fcdf03a4d48a06c8a4a068432a4f3abd569b848d
Chanic Panic <51764816+chanicpanic@users.noreply.github.com>
2025-07-21 21:21:50 -0700

    Qwertigraphy: Prepare for uniform dictionaries

--- qwertigraphy/update.sh
+++ qwertigraphy/update.sh
@@ -6,9 +6,9 @@ fi
 
 git -C qwertigraphy-clone checkout master
 git -C qwertigraphy-clone pull
-cp -v qwertigraphy-clone/qwertigraph/dictionaries/anniversary_core.csv anniversary-core/anniversary_core.csv
-cp -v qwertigraphy-clone/qwertigraph/dictionaries/anniversary_supplement.csv anniversary-supplement/anniversary_supplement.csv
-git add anniversary-core/anniversary_core.csv anniversary-supplement/anniversary_supplement.csv
+cp -v qwertigraphy-clone/qwertigraph/dictionaries/anniversary_uniform_core.csv anniversary-core/anniversary_uniform_core.csv
+cp -v qwertigraphy-clone/qwertigraph/dictionaries/anniversary_uniform_supplement.csv anniversary-supplement/anniversary_uniform_supplement.csv
+git add anniversary-core/anniversary_uniform_core.csv anniversary-supplement/anniversary_uniform_supplement.csv
 echo Qwertigraphy: Update to $(git -C qwertigraphy-clone rev-parse --short HEAD) > commit-message.txt
 echo To complete the update, run
 echo git commit -F qwertigraphy/commit-message.txt
