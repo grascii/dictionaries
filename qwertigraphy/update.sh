4772289471921a99001feffd72f9a54d4da9a681
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2026-03-10 18:55:41 -0700

    Qwertigraphy: Update to c0d698d (#18)
    The location of the csv files moved and they no longer include column names.

--- qwertigraphy/update.sh
+++ qwertigraphy/update.sh
@@ -6,8 +6,8 @@ fi
 
 git -C qwertigraphy-clone checkout master
 git -C qwertigraphy-clone pull
-cp -v qwertigraphy-clone/qwertigraph/dictionaries/anniversary_uniform_core.csv anniversary-core/anniversary_uniform_core.csv
-cp -v qwertigraphy-clone/qwertigraph/dictionaries/anniversary_uniform_supplement.csv anniversary-supplement/anniversary_uniform_supplement.csv
+cp -v qwertigraphy-clone/qwertigraph/pychorder/dictionaries/anniversary_uniform_core.csv anniversary-core/anniversary_uniform_core.csv
+cp -v qwertigraphy-clone/qwertigraph/pychorder/dictionaries/anniversary_uniform_supplement.csv anniversary-supplement/anniversary_uniform_supplement.csv
 git add anniversary-core/anniversary_uniform_core.csv anniversary-supplement/anniversary_uniform_supplement.csv
 echo Qwertigraphy: Update to $(git -C qwertigraphy-clone rev-parse --short HEAD) > commit-message.txt
 echo To complete the update, run
